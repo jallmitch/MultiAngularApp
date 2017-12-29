a0e3c216b9076e9135c9ddac9d06f87ecc0ea889
Jesse Mitchell <jallmitch@yahoo.com>
2017-12-29 13:12:53 -0700

    Add Settings Page to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { SettingsPage } from '../pages/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,6 +23,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
+      { title: 'Settings', component: SettingsPage },
       { title: 'List', component: ListPage }
     ];
 
